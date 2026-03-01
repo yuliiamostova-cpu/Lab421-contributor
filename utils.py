66c0e2dbaa6a9462e80d7ac424a1c26a0986b3fc
Yuliia Mostova <yulliamostovaya@gmail.com>
2026-03-01 22:52:57 +0200

    Changed utils.py

--- utils.py
+++ utils.py
@@ -3,3 +3,14 @@ def factorial(n: int) -> int:
     for i in range(2, n + 1):
         res *= i
     return res
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+    if n % 2 == 0:
+        return n == 2
+    d = 3
+    while d * d <= n:
+        if n % d == 0:
+            return False
+        d += 2
+    return True
